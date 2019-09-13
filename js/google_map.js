abf2188c967dbd65292f7ca94a8b6239fd2eb369
jainhere <njain_be16@thapar.edu>
2019-09-13 19:46:35 +0530

    Changes

--- js/google_map.js
+++ js/google_map.js
@@ -5,14 +5,10 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(30.3544, 76.3626);
+    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
     // 39.399872
     // -8.224454
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        title: 'Your Location',
-        map: map
-    });
+    
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 7,
@@ -32,7 +28,7 @@ function init() {
     var mapElement = document.getElementById('map');
 
     // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions,marker);
+    var map = new google.maps.Map(mapElement, mapOptions);
     
     var addresses = ['Brooklyn'];
 
